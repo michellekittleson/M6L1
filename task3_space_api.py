264fe042f7d2aa669891c62fbcbed80860c6d91a
michellekittleson <michelle.kittleson@gmail.com>
2025-04-16 10:05:33 -0500

    another commit

--- task3_space_api.py
+++ task3_space_api.py
@@ -18,22 +18,26 @@ def fetch_planet_data():
 fetch_planet_data()
 
 
-def find_heaviest_planet(planets):
+def find_heaviest_planet():
     url = "https://api.le-systeme-solaire.net/rest/bodies/"
     response = requests.get(url)
-    planets = response.json()['bodies']
+    bodies = response.json()['bodies']
 
+    heaviest_name = None
+    heaviest_mass = 0
+
+    for body in bodies:
+        if body.get('isPlanet') and body.get('mass'):
+            m = body['mass']
+            mass_kg = m['massValue'] * 10 **m['massExponent']
+            if mass_kg > heaviest_mass:
+                heaviest_mass = mass_kg
+                heaviest_name = body.get('englishName', body.get('name'))
     
+    return heaviest_name, heaviest_mass
 
-    for planet in planets:
-        planet_masses = []
-        if planet['isPlanet']:
-            planet_masses.append(planet['mass'])
-    
-    max_mass = max(planet_masses)
-    return max_mass
-
-print(f"The heaviest planet is {name} has a mass of {find_heaviest_planet(planets)} kg.")
+name, mass = find_heaviest_planet()
+print(f"The heaviest planet is {name} has a mass of {mass:.2e} kg.")
 
 
 
